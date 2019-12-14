11a0dac6f2fc613f58ca9aac3d80231c6ffcfe50
Mashima <52222646+zimashima@users.noreply.github.com>
2019-12-13 21:16:35 -0500

    fixed wording

--- src/components/Result.js
+++ src/components/Result.js
@@ -15,7 +15,7 @@ const Result = props => {
             <div>
                 {
                     props.props.isCorrect? 
-                    <h3 className="text-success text-center">Good Job!</h3> : <h3 className="text-danger text-center">Not so smart, aren't you? :P </h3>
+                    <h3 className="text-success text-center">Look who's so smart!</h3> : <h3 className="text-danger text-center">Not so smart, aren't you? :P </h3>
                 }
                 <span class="btn btn-primary" onClick={handleNext}>Next Question</span>
             </div>
